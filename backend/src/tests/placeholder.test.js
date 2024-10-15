b1e0d3a5682666a567753fc3d471a56ed43eb4af
Andreas Ink <andreasink17@gmail.com>
2024-10-14 23:10:11 -0400

    Create placeholder.test.js

---
+++ backend/src/tests/placeholder.test.js
@@ -0,0 +1,8 @@
+// test/placeholder.test.js
+const assert = require('chai').assert;
+
+describe('Placeholder Test', function() {
+  it('should pass this placeholder test', function() {
+    assert.equal(1, 1);
+  });
+});
