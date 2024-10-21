541d4c9ad47a6cc0270ef0d330420c029344d74d
micahvanm <n01454650@unf.edu>
2024-10-21 14:51:03 -0400

    First working firebase access

--- frontend/src/app/uploading/uploading.component.ts
+++ frontend/src/app/uploading/uploading.component.ts
@@ -1,20 +1,34 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgFor, NgForOf } from '@angular/common';
 import { UploadsService } from '../services/uploads.service';
-import {} from '../../../../backend/src/firebase/firebaseConnection';
+import { getCases } from '../../../../backend/src/firebase/firebaseConnection';
+import { DocumentData } from 'firebase/firestore';
+
 
 @Component({
   selector: 'app-uploading',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgFor],
   templateUrl: './uploading.component.html',
   styleUrls: ['./uploading.component.css']
 })
 export class UploadingComponent {
 
   filesToUpload: File[] | null = null;
+  testDB: Promise<DocumentData[]> | null = null;
+  testDBData: DocumentData[] = [];
 
-  constructor(private uploadService: UploadsService) {}
+  constructor(private uploadService: UploadsService) {
+    // this.testDB = getCases()
+    this.testfunc()
+  }
+
+  async testfunc() {
+    await getCases().then(result => this.testDBData?.push(result))
+    this.testDBData.forEach(item => {
+      console.log(item)
+    })
+  }
 
   handleFileInput(event: Event) {
     const input = event.target as HTMLInputElement;
